fbbd2fd9bf5a5b76abde5aac27e2c5244c6ba074
sahemr <sahinemre810@gmail.com>
2021-06-10 15:34:29 +0200

    Added second update

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,5 +2,8 @@ public class HelloTest {
     public static void main(String[] args) {
 
         System.out.println("Hello Test");
+
+        System.out.println("About to share project on GitHup");
+
     }
 }
